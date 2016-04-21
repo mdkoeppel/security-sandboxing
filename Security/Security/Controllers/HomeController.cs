4db4578eae7e376123528c1f48252124f0af94e9
mdkoeppel <mk02561@georgiasouthern.edu>
2016-04-21 15:43:33 -0400

    First commit

---
+++ Security/Security/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Security.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Title = "| Welcome! |";
+
+            return View();
+        }
+    }
+}
---
+++ Security/Security/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Security.WebApiApplication" Language="C#" %>
